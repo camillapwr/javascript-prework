34b3d81a4a908d6a66d29281c243d6d378b9fe8a
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-03-25 15:56:17 +0100

    Set var as exercise1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+var playerMove, computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałeś' + playerMove + '! Mój ruch to kamień, więc wygrałeś');
