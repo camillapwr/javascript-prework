b08ae12ba1088ae0fd3bfb40c3e435b280796612
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-03-25 17:47:27 +0100

    Change in script exercise2

--- js/script.js
+++ js/script.js
@@ -12,18 +12,10 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-if (randomNumber == '2') {
+} else if (randomNumber == '2') {
   computerMove = 'papier';
-} else {
-  computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-if (randomNumber == '3') {
-  computerMove = 'nozyce';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
 } else {
   computerMove = 'nieznany ruch';
 }
